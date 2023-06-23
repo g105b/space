b09ff103be3d3f2a1e1d17adf730337b7bde07f3
Greg Bowler <greg.bowler@g105b.com>
2023-06-23 19:25:43 +0100

    wip: ui changes

---
+++ class/Graphics/Palette.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Space\Graphics;
+
+enum Palette:int {
+	case BLACK = 0x000000;
+	case DARK_BLUE = 0x1D2B53;
+	case PURPLE = 0x7E2553;
+	case DARK_GREEN = 0x008751;
+	case BROWN = 0xAB5236;
+	case DARK_GREY = 0x5F574F;
+	case LIGHT_GREY = 0xC2C3C7;
+	case WHITE = 0xFFF1E8;
+	case RED = 0xFF004D;
+	case ORANGE = 0xFFA300;
+	case YELLOW = 0xFFEC27;
+	case GREEN = 0x00E436;
+	case BLUE = 0x29ADFF;
+	case LAVENDER = 0x83769C;
+	case PINK = 0xFF77A8;
+	case LIGHT_PEACH = 0xFFCCAA;
+}
